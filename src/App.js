7d621d56d064fc3c59de5cf8bb334fe12cbbb77b
ruffbigloud <ruff@bigloud.com>
2025-06-06 13:58:48 -0700

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div>
+      <h1>Working Dashboard</h1>
+      <p>If you see this, everything is fixed!</p>
+    </div>
+  );
+}
+
+export default App;
